fa3421310ac872adf2f4520a8ac8a83a4ccb21d7
Juyeop Kim <juyeopkim@yonsei.ac.kr>
2023-03-04 16:04:17 +0900

    yupeeee-pytools-0.1.1 update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yupeeee-pytools",
-    version="0.1",
+    version="0.1.1",
     description="",
     long_description="",
     author="Juyeop Kim",
